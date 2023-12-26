be4c783eb868d30a37a2e17f02047edb9fb20c62
alexm3133 <alexandrumateiionut@gmail.com>
2023-12-26 08:32:09 +0100

    implementing the led bar

--- lcd_rtos_implemented/lcd_rtos.ino
+++ lcd_rtos_implemented/lcd_rtos.ino
@@ -3,6 +3,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <freertos/semphr.h>
 
 // Network credentials
 const char* ssid = "privatered";
@@ -18,21 +19,27 @@ WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
+SemaphoreHandle_t xSemaphoreLCD; // Semáforo para el LCD
+
 float distance1 = 0.0;
 float distance2 = 0.0;
 const float detectionRange = 100.0; // Distancia de detección en cm
 
-// Declaración de las funciones de las tareas
 void WiFiTask(void *pvParameters);
 void MQTTTask(void *pvParameters);
 void LCDDisplayTask(void *pvParameters);
+void mqttCallback(char* topic, byte* payload, unsigned int length);
+void checkForPresenceAndDirection(float d1, float d2, float range);
+void reconnectMQTT();
+void lcdPrint(String message, int line);
 
 void setup() {
   Serial.begin(115200);
   lcd.init();
   lcd.backlight();
 
-  // Creación de tareas
+  xSemaphoreLCD = xSemaphoreCreateMutex(); // Crear semáforo para el LCD
+
   xTaskCreate(WiFiTask, "WiFi Task", 10000, NULL, 1, NULL);
   xTaskCreate(MQTTTask, "MQTT Task", 10000, NULL, 1, NULL);
   xTaskCreate(LCDDisplayTask, "LCD Display Task", 10000, NULL, 1, NULL);
@@ -45,15 +52,15 @@ void loop() {
 void WiFiTask(void *pvParameters) {
   for (;;) {
     if (WiFi.status() != WL_CONNECTED) {
-      lcdPrint("Connecting to WiFi");
+     // lcdPrint("Connecting to WiFi",1);
       WiFi.begin(ssid, password);
       while (WiFi.status() != WL_CONNECTED) {
         vTaskDelay(500 / portTICK_PERIOD_MS);
       }
       Serial.println("\nConnected to WiFi");
-      lcdPrint("IP: " + WiFi.localIP().toString());
+   //   lcdPrint("IP: " + WiFi.localIP().toString(),2);
     }
-    vTaskDelay(10000 / portTICK_PERIOD_MS); // Revisar la conexión cada 10 segundos
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
   }
 }
 
@@ -66,7 +73,7 @@ void MQTTTask(void *pvParameters) {
       reconnectMQTT();
     }
     mqttClient.loop();
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // Loop de MQTT cada segundo
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
@@ -75,7 +82,7 @@ void LCDDisplayTask(void *pvParameters) {
     if (distance1 > 0 && distance2 > 0) {
       checkForPresenceAndDirection(distance1, distance2, detectionRange);
     }
-    vTaskDelay(2000 / portTICK_PERIOD_MS); // Actualizar el display cada 2 segundos
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
   }
 }
 
@@ -93,39 +100,70 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
 }
 
 void checkForPresenceAndDirection(float d1, float d2, float range) {
-  if (d1 <= range || d2 <= range) {
-    if (d1 < d2) {
-      lcdPrint("izquierda: " + String(d1) + " cm");
+    if (d1 <= range && d2 <= range) {
+        float sensorDistance = 30.0; // en cm
+        float angleBetweenSensors = 45.0; // en grados
+
+        float cosC = (d1 * d1 + d2 * d2 - sensorDistance * sensorDistance) / (2 * d1 * d2);
+
+        // Comprobar que cosC está en el rango válido para el arcocoseno
+        if (cosC > 1.0) cosC = 1.0;
+        if (cosC < -1.0) cosC = -1.0;
+
+        float angleC = acos(cosC);
+
+        float leftPercentage = 0.0;
+        float rightPercentage = 0.0;
+
+        if (angleC < PI / 2) {
+            leftPercentage = 100.0 * (1 - angleC / (PI / 2));
+        } else {
+            rightPercentage = 100.0 * (1 - (PI - angleC) / (PI / 2));
+        }
+
+        String line1 = "Left: " + String(leftPercentage, 2) + "%";
+        String line2 = "Right: " + String(rightPercentage, 2) + "%";
+
+        lcdPrint(line1, 1);
+        lcdPrint(line2, 2);
     } else {
-      lcdPrint("derecha: " + String(d2) + " cm");
+        lcdPrint("Fuera de rango", 1);
+        lcdPrint("", 2);
     }
-  } else {
-    lcdPrint("No hay persona");
-  }
 }
+
 
 void reconnectMQTT() {
   while (!mqttClient.connected()) {
     Serial.print("Attempting MQTT connection...");
-    lcdPrint("Connecting to MQTT");
+   // lcdPrint("Connecting to MQTT",1);
     if (mqttClient.connect("ESP32Client")) {
       Serial.println("connected");
-      lcdPrint("MQTT Connected");
+    //  lcdPrint("MQTT Connected",2);
       mqttClient.subscribe(mqtt_topic1);
       mqttClient.subscribe(mqtt_topic2);
     } else {
       Serial.print("failed, rc=");
       Serial.print(mqttClient.state());
       Serial.println(" try again in 5 seconds");
-      lcdPrint("MQTT Connect failed");
+    //  lcdPrint("MQTT Connect failed",2);
       delay(5000);
     }
   }
 }
 
-void lcdPrint(String message) {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(message);
-  delay(2000); 
+void lcdPrint(String message, int line) {
+  if (xSemaphoreTake(xSemaphoreLCD, portMAX_DELAY)) {
+    lcd.setCursor(0, line - 1);
+    lcd.print(message);
+    for (int i = message.length(); i < 16; i++) {
+      lcd.print(" ");
+    }
+    xSemaphoreGive(xSemaphoreLCD);
+    // Reducir o eliminar este delay según sea necesario
+    delay(100); // Ajusta este valor según tus necesidades
+  }
 }
+
+
+
